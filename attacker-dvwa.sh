a00fc6ad975cab64e12e5af0df82d897df9f8d55
Andre Fernandes <andre.fernandes86@gmail.com>
2020-04-09 18:10:55 +0400

    Update attacker-dvwa.sh

--- attacker-dvwa.sh
+++ attacker-dvwa.sh
@@ -1,7 +1,12 @@
 sudo su
 apt-get update
+sleep 15
 apt-get install -y curl
+sleep 10
 apt-get install -y wget
+sleep 10
 apt-get install -y python
+sleep 20
 curl -fsSL https://get.docker.com | sh
+sleep 20
 docker run -d --rm --name demo-dvwa -d -p 80:80 andrefernandes86/demo-dvwa
